a8e878e38e8d010cf0dd8c9290b95713fc39cfc1
Maurice <maurice.nzai@gmail.com>
2022-10-07 09:09:43 +0000

    division function

---
+++ 7-div.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * div - divides second top element by the top element
+ * @stack: stack_t variable from which to divied elements
+ * @line_number: Position in stack
+ */
+
+void divsion(stack_t **stack, unsigned int line_number)
+{
+	unsigned int result;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = (*stack)->next->n / (*stack)->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, result);
+}
